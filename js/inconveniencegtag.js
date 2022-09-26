98725c30df8b6b4f4175930fbece8a95c257fcbb
Alexandra Greenspan <polarwishes@gmail.com>
2022-09-26 16:56:49 -0700

    Update inconveniencegtag.js

--- js/inconveniencegtag.js
+++ js/inconveniencegtag.js
@@ -1,9 +1,5 @@
-<!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-LY3PH9920Y"></script>
-<script>
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
 
   gtag('config', 'G-LY3PH9920Y');
-</script>
